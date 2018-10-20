5adc2ef76e0e2e147041fbb42d3b21d4cec65b23
7noel <noel.logan@gmail.com>
2018-10-19 23:33:25 -0500

    app remove excel

--- config/app.php
+++ config/app.php
@@ -180,8 +180,6 @@ return [
 
         Collective\Html\HtmlServiceProvider::class,
 
-        Maatwebsite\Excel\ExcelServiceProvider::class,
-
     ],
 
     /*
@@ -232,8 +230,6 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-
-        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
