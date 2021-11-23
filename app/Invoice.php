6a7f4158add14df519056edfe4589c1d27a3ce64
7noel <noel.logan@gmail.com>
2021-11-23 00:57:00 -0500

    3333

--- app/Invoice.php
+++ app/Invoice.php
@@ -6,11 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invoice extends Model
 {
-	//protected $connection = 'masaki';
 	protected $table = 'ventas';
 	protected $primaryKey = 'NroVenta';
 	public $timestamps = false;
-	protected $fillable = ['status_sunat', 'email', 'email1', 'email2', 'respuesta_sunat', 'respuesta_anulacion'];
+	protected $fillable = ['CondPago', 'FechaVence', 'Dias', 'status_sunat', 'email', 'email1', 'email2', 'respuesta_sunat', 'respuesta_anulacion'];
 
 
 }
